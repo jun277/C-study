f25d64e6abadcbd13516232a65e9e9a7fa5d3a34
jun277 <fad3@163.com>
2020-05-05 15:36:23 +0800

    fix code

--- 4.16varwid.c
+++ 4.16varwid.c
@@ -1,5 +1,5 @@
 /* 4.16 varwid.c -- 使用可变宽度输出字段 */
-#include <stdio.h>
+/*#include <stdio.h>
 int main(void)
 {
   unsigned width, precision;
@@ -15,4 +15,22 @@ int main(void)
   printf("Done!\n");
 
   return 0;
+}
+*/
+#include <stdio.h>
+int main(void)
+{
+    unsigned width,precision;
+    int number = 256;
+    double weight = 242.5;
+
+    printf("Enter a field width:\n");
+    scanf("%d",&width);
+    printf("The number is:%*d \n",width,number);
+    printf("Now enter a width and a precision: \n");
+    scanf("%d %d",&width,&precision);
+    printf("Weight = %*.*f \n",width,precision,weigth);
+    printf("Done!\n");
+
+    return 0;
 }
